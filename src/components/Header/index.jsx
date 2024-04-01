5df187c60f9b884995d0b2c186726a1be6ba6eda
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-04-01 15:31:30 -0700

    added comment question to Header/index

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,7 @@ export const Header = (props) => {
   return (
     <header className="flex-row space-between px-1">
       <h1>Dani Drury</h1>
+      {/* why props.children? In App.jsx, we're not passing props to Header... what does this do? */}
       {props.children}
     </header>
   );
