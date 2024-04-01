7789d5b777f7c7208bcbcf0604e80e99f446e234
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-04-01 15:31:16 -0700

    built out About and Contact components, added email validation helper function and profile pic

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
+
+export const Contact = () => {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  }
+  
+  const handleChange = (event) => {
+    if (event.target.name === 'email') {
+      const isValid = validateEmail(event.target.value);
+      if  (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!event.target.value.length) {
+        setErrorMessage(`${event.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [event.target.name]: event.target.value });
+      console.log('Handle Form', formState);
+    }
+  };
+
+  return (
+    <section>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="email">Email Address:</label>
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange}/>
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className='error-text'>{errorMessage}</p>
+          </div>
+        )}
+        <button type='submit'>Submit</button>
+      </form>
+    </section>
+  );
+}
