f6fb3424177204c124609fe423fc1641429188ee
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-04-01 13:08:39 -0700

    Nav/index.jsx edit - separated out <li> item creation from return jsx

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,17 +4,19 @@ import { capitalizeFirstLetter } from '../../utils/helpers';
 export const Nav = ({ currentPage }) => {
   const pages = ['portfolio', 'contact', 'resume'];
 
+  const listItems = pages.map((Page) => (
+    <li className={`mx-5 ${currentPage === `/${Page}` && 'navActive'}`} key={Page}>
+      <Link to={`/${Page}`}>{capitalizeFirstLetter(Page)}</Link>
+    </li>
+    ));
+
   return (
     <nav>
       <ul>
         <li className={`mx-5 ${currentPage === '/' && 'navActive'}`} key='about'>
           <Link to='/'>About</Link>
         </li>
-        {pages.map((Page) => (
-          <li className={`mx-5 ${currentPage === `/${Page}` && 'navActive'}`} key={Page}>
-            <Link to={`/${Page}`}>{capitalizeFirstLetter(Page)}</Link>
-          </li>
-        ))}
+        {listItems}
       </ul>
     </nav>
   );
