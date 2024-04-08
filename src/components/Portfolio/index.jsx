4b639297846e8804aa8bd75c2e27c5b436386aa0
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-04-07 20:16:29 -0700

    edited image paths

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -6,37 +6,37 @@ const projectArr = [
     name: "PropertyPlus Care",
     github: "https://github.com/stephen-scheiman/PropertyPlus-Care",
     url: "https://property-plus-care-dab80668cb03.herokuapp.com/",
-    img: "public/assets/img/proppluscare.png"
+    img: "/assets/img/proppluscare.png"
   },
   {
     name: "Entertainment Search",
     github: "https://github.com/DaniDrury/entertainment-search",
     url: "https://danidrury.github.io/entertainment-search/",
-    img: "public/assets/img/entertainmentsearch.png"
+    img: "/assets/img/entertainmentsearch.png"
   },
   {
     name: "Dani's Tech Blog",
     github: "https://github.com/DaniDrury/tech-blog",
     url: "https://dnd-tech-blog-ad0fb839885d.herokuapp.com/",
-    img: "public/assets/img/techblog.png"
+    img: "/assets/img/techblog.png"
   },
   {
     name: "Weather Dashboard",
     github: "https://github.com/DaniDrury/weather-dashboard",
     url: "https://danidrury.github.io/weather-dashboard/",
-    img: "public/assets/img/weatherdashboard.png"
+    img: "/assets/img/weatherdashboard.png"
   },
   {
     name: "Work Day Scheduler",
     github: "https://github.com/DaniDrury/work-day-scheduler",
     url: "https://danidrury.github.io/work-day-scheduler/",
-    img: "public/assets/img/work-day-scheduler.png"
+    img: "/assets/img/work-day-scheduler.png"
   },
   {
     name: "Coding Quiz Game",
     github: "https://github.com/DaniDrury/quiz-game",
     url: "https://danidrury.github.io/quiz-game/",
-    img: "public/assets/img/quizgame.png"
+    img: "/assets/img/quizgame.png"
   },
 ]
 
